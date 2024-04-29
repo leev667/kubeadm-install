b1e54b540e26ceaef672ffbb5370b597ec6d9de7
Cloud User <rocky@lee-bastion.symphony.local>
2024-04-29 15:38:10 +0000

    Worker install updated

---
+++ rocky_kubeadm_containerd_calico_worker_install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Update OS
+dnf update -y
+
+# Configure kubernetes repo
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+
+# Installing Container Runtime prerequisites
+# Forwarding IPv4 and letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Verify modules are loaded
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
+#Verify that the net.bridge.bridge-nf-call-iptables, net.bridge.bridge-nf-call-ip6tables, and net.ipv4.ip_forward system variables are set to 1
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+#Disable SELinux
+setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+getenforce
+
+#Install kubectl, kubeadm, kubelet
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+#Install the latest version of docker from repository rather than rocky
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+#Enable and start services
+systemctl enable kubelet; systemctl start kubelet; systemctl enable docker; systemctl start docker
+
+#Disable swap
+swapoff -a
+
+#Change cgroup setting to be true in containerd for systemd
+containerd config default | tee /etc/containerd/config.toml
+sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd
+
+#kubeadm initiliase apiserver address should be the same as main interface on master
+#kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.1.72 | tee kubeadm-init.out
+
+read -p "Enter token: " token
+read -p "Enter discovery-token-ca-cert-hash: " cert_hash
+read -p "Enter Control Plane IP: " cp_ip
+#Variables
+TOKEN="$token"
+CERT_HASH="$cert_hash"
+CP_IP="$cp_ip"
+#Join
+kubeadm join {$CP_IP}:6443 --token $TOKEN --discovery-token-ca-cert-hash $CERT_HASH
+
+#Set up config as regular user
+#exit
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Install calico
+sudo dnf install wget
+wget https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f calico.yaml
+
+#bash completion
+sudo dnf install bash-completion -y
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> $HOME/.bashrc
